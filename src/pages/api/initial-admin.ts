5dec01edeb79f0218c6c87f01d1e6ee15c905dbe
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-22 14:28:39 -0500

    moved to initial-admin

--- src/pages/api/initial-admin.ts
+++ src/pages/api/initial-admin.ts
@@ -1,9 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createInitialAdmin } from "../../utils/initialAdminCreation";
+import { AdminData } from "../../database/interfaces/admin";
+import { createAdmin } from "../../database/operations/admin";
 
 export default async function InitialAdmin(req:NextApiRequest, res:NextApiResponse) {
 
-    createInitialAdmin()
+    const data:AdminData = {
+        username: "test",
+        email: "test",
+        password: "test"
+    }
+
+    await createAdmin(data)
 
     return res.json({msg: "hi"})
 }
