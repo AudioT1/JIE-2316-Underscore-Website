207697c3846d4c8d2082310ade048502781529f9
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-22 13:52:05 -0500

    removed prev setup stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import styles from "../styles/Home.module.css"
-import Switch from "@mui/material/Switch"
-
-const label = {inputProps: {"aria-label": "Switch Demo"}}
 
 export default function Home() {
   
 	return (
-		<div className={styles.container}>
-			<div>
-				<span>With default theme:</span>
-			</div>
-			<Switch {...label} defaultChecked />
-			<Switch {...label} />
-			<Switch {...label} disabled defaultChecked />
+		<div>
+			hello world
 		</div>
 	)
 }
