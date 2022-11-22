4a8ba0bb81acb7e1e31a347556324b42a5249f82
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-22 14:43:32 -0500

    commented createIntialAdmin code

--- src/pages/api/initial-admin.ts
+++ src/pages/api/initial-admin.ts
@@ -1,7 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { AdminData } from "../../database/interfaces/admin";
 import { createAdmin } from "../../database/operations/admin";
+import bcrypt from "bcryptjs"
 
+export default async function InitialAdmin(req:NextApiRequest, res:NextApiResponse) {
+    return res.json({msg: "Hello World"})
+}
+
+// The following should only be uncommented for creating a new root admin.
+// This SHOULD NOT BE USED IN PRODUCTION
+/*
 export default async function InitialAdmin(req:NextApiRequest, res:NextApiResponse) {
 
     const data:AdminData = {
@@ -10,7 +18,17 @@ export default async function InitialAdmin(req:NextApiRequest, res:NextApiRespon
         password: process.env.ADMIN_ROOT_PASSWORD
     }
 
+    const hashedPassword = await new Promise<string>((resolve, reject) => {
+        bcrypt.hash(data.password, 10, (err, hash) => {
+            if (err) reject(err)
+            resolve(hash)
+        })
+    })
+
+    data.password = hashedPassword
+
     await createAdmin(data)
 
-    return res.json({msg: "hi"})
+    return res.json({msg: "Created Initial Admin Account"})
 }
+*/
