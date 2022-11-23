97af768831f3733b4c6583cd3a86eea6f60bedef
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-22 23:07:24 -0500

    incrementing daily analytics

--- src/database/operations/analyticsUser.ts
+++ src/database/operations/analyticsUser.ts
@@ -1,5 +1,6 @@
 import client from "../fauna"
 import { query as q } from "faunadb"
+import { incrementAnalyticsDailyInnerQuery } from "./analyticsDaily"
 
 function getAnalyticsUserInnerQuery(randomId:string) {
     return q.If(
@@ -16,20 +17,23 @@ function getAnalyticsUserInnerQuery(randomId:string) {
 export async function incrementPageVisited(randomId:string, page:string) {
 
     return await client.query(
-        q.Let(
-            {
-                user: getAnalyticsUserInnerQuery(randomId)
-            },
-            q.Update(q.Select("ref", q.Var("user")), {data: {
-                pageVisitFrequencies: {
-                    [page]: q.If(
-                        q.ContainsField(page, q.Select(["data", "pageVisitFrequencies"], q.Var("user"))),
-                        q.Add(q.Select(["data", "pageVisitFrequencies", page], q.Var("user")), 1),
-                        1
-                    ),
+        q.Do(
+            q.Let(
+                {
+                    user: getAnalyticsUserInnerQuery(randomId)
                 },
-                lastVisit: q.Now()
-            }})
+                q.Update(q.Select("ref", q.Var("user")), {data: {
+                    pageVisitFrequencies: {
+                        [page]: q.If(
+                            q.ContainsField(page, q.Select(["data", "pageVisitFrequencies"], q.Var("user"))),
+                            q.Add(q.Select(["data", "pageVisitFrequencies", page], q.Var("user")), 1),
+                            1
+                        ),
+                    },
+                    lastVisit: q.Now()
+                }})
+            ),
+            incrementAnalyticsDailyInnerQuery(page)
         )
     )
 }
