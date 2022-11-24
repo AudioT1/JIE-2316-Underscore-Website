9f527a43282542e1b75a42889ab3d8e95ddd7fe6
Manuel Roglan <manueljoseph113@gmail.com>
2022-11-24 15:48:22 -0500

    updated theme

---
+++ src/pages/admin/login.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Login() {
+
+    return (
+        <>
+            <Head>
+                <title>Admin Login</title>   
+            </Head> 
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,39 @@
-import { createTheme } from "@mui/material";
+import { createTheme, PaletteOptions } from "@mui/material";
+
+declare module "@mui/material/styles" {
+
+    interface Palette {
+        secondaryBg?: {
+            header?: string;
+        };
+    }
+
+    interface PaletteOptions {
+        secondaryBg?: {
+            header?: string;
+        };
+    }
+}
 
 export const theme = createTheme({
     palette: {
+        mode: "light",
         primary: {
-            main: "#fcba03"
-        }
+            main: "#D2B49C"
+        },
+        error: {
+            main: "#FF2414"
+        },
+        text: {
+            primary: "#535040",
+            secondary: "#F1F0E2"
+        },
+        background: {
+            default: "#F1F0E2",
+            paper: "#E7E7E7"
+        },
+        secondaryBg: {
+            header: "#535040"
+        } 
     }
 })
