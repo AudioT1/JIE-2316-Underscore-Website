f878cfcfc75a6dafbcbf98fdd4d693feba73f5bd
Manuel Roglan <manueljoseph113@gmail.com>
2023-02-11 09:07:33 -0500

    updated header and buttons

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Box, Grid, Toolbar, Typography, IconButton, useMediaQuery,
   Drawer, } from "@mui/material";
 import Link from "next/link";
+import { OrangePrimaryButton } from "../misc/buttons";
 
 
 
@@ -11,49 +12,42 @@ export default function Header() {
     <Box mb={3}>
       <AppBar position="static" sx={{backgroundColor: "secondaryBg.header"}}>
           <Toolbar>
-          <Grid container spacing={0} alignItems="start" direction="row">
-              <Grid item xs={3}>
+          <Grid container spacing={0} alignItems="center" direction="row">
+              <Grid item>
                 <Typography variant="h4" color="orange">
                     <Link href="/" >audioT</Link>
-                      
                   </Typography>
                 
               </Grid>
-              <Grid item xs={2}>
-              <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
-                <Typography variant="body1" color="text.primary">
-                      <Link href="/our-mission"> Our Mission</Link>
-                    </Typography>
-                </Box>
-                
-                 
-                
+              <Grid item flex={1} />
+              <Grid item mx={3}>
+                <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
+                  <Typography variant="body1" color="text.primary">
+                        <Link href="/our-mission"> Our Mission</Link>
+                      </Typography>
+                  </Box>
               </Grid>
-              <Grid item xs={2}>
-              <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
+              <Grid item mx={3}>
+                <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
                   <Typography variant="body1" color="text.primary">
                     <Link href="/">Technology</Link>
                   </Typography>
                 </Box>
-
               </Grid>
-              <Grid item xs={2}>
-              <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
+              <Grid item mx={3}>
+                <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
                   <Typography variant="body1" color="text.primary">
                     <Link href="/about-us">About Us</Link>
                   </Typography>
                 </Box>
-                
               </Grid>
-              <Grid >
-                <Box  sx={{ p: 2, backgroundColor: 'orange' }} borderRadius={3}>
-                  <Typography  color="text.secondary">
-                     <Link href="/contact-us"> Contact Us</Link>
-                  </Typography>
-                    
-                </Box>
+              <Grid item mx={3}>
+                <Link href="/contact-us">
+                  <OrangePrimaryButton>
+                    Contact Us
+                  </OrangePrimaryButton>
+                </Link>
               </Grid>
-
             </Grid>
           </Toolbar>
       </AppBar>
