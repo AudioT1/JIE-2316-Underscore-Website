ee7412c83b5600971e443c179955108a1465214e
thejulia1 <90807530+thejulia1@users.noreply.github.com>
2023-02-12 21:56:09 -0500

    top section of mission page

---
+++ src/components/missionPage/Composite.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import FirstSection from "./FirstSection";
+
+export default function Composite() {
+
+    return (
+        <Box>
+            <FirstSection />
+        </Box>
+    )
+}
