ed0aa525ddf084ca621cceda4c627b1c0fa5ad6e
thejulia1 <90807530+thejulia1@users.noreply.github.com>
2023-02-15 22:33:34 -0500

    edit page layout to updated figma

--- src/components/missionPage/SecondSection.tsx
+++ src/components/missionPage/SecondSection.tsx
@@ -8,48 +8,39 @@ export default function SecondSection() {
     const smallScreen = useMediaQuery(theme.breakpoints.down('md'))
 
     return (
-        <Box sx={{ background:`linear-gradient(to bottom,#ffffff,#bdbdbd)`, height: 5000,backgroundPosition: 'center', backgroundRepeat: 'no-repeat',backgroundSize: 'cover',position: 'relative'}}>
-            
-            <Box sx={{backgroundColor: "background.default", height: 900}}  px={2} py={10} mt={smallScreen? "30px":"20px"} position="absolute" width="95%" right="0%">
-                <Grid container spacing={0} alignItems="center" direction="row">
-                    <Grid item width="50%" >
+        <Box  sx={{ background:`linear-gradient(to bottom,#F2F5FA,#9e9e9e)`, height: 2200,backgroundPosition: 'center', backgroundRepeat: 'no-repeat',backgroundSize: 'cover',position: 'relative'}}>
+            <Grid container spacing={0} alignItems="center" direction="row">
+                    <Grid item >
                         <Box sx={{height: 500}} textAlign="left" >
-                            <Typography px={15}   sx={{fontWeight: 'bold', letterSpacing: 1}} variant={smallScreen ? 'h5' : 'h4'} color="secondary.main">
+                            <Typography width="50%" pl={40}  pt={15}   sx={{ letterSpacing: 1}} variant={smallScreen ? 'h5' : 'h4'} color="secondary.main">
                             Learning From the Best
                             </Typography> 
-                            <Typography px={15} py={3}  sx={{  }} variant={smallScreen ?  'body2' : 'body1'} color="secondary.main">
+                            <Typography  width="50%"  pl={40} pt={5}  sx={{  }} variant={smallScreen ?  'body2' : 'body1'} color="secondary.main">
                             For centuries, farmers have used their experience to learn and adapt to the needs of their animals. 
                             Their dedication to caring for animals helps to feed the world.
                             </Typography> 
-                            <Typography px={15} py={3}  sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
+                            <Typography width="50%" pl={40} pt={5}  sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
                             The amount of specialized knowledge needed to be a poultry farmer is surprisingly great. Just by 
                             hearing one of their chickens, a poultry farmer can tell if they’re healthy, happy, hungry, or sick. 
                             It may seem instinctual to some, but that level of adaptation is made possible by the supercomputer
                             between our ears -- our brains!
                             </Typography> 
-                            <Typography px={15} py={3}  sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
+                            <Typography width="50%" pl={40} pt={5}  pb={15} sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
                             We can’t replicate what farmers do, but we can strive toke ma their work easier in this rapidly changing world.
                             </Typography> 
+                            
+                            <Grid item width="100%" px={10}>
+                                <Box sx={{backgroundColor: "text.primary", height: 750, borderRadius: '30px'}} px={50}>
+
+                                </Box>
+                            </Grid>
 
                         </Box>
+                        
                     </Grid>
-                    <Grid item width="50%">
-                    <Box sx={{height: 500}} textAlign="left" >
-                        <Typography px={15} py={0}  sx={{  }} variant={smallScreen ? 'h5' : 'body1'} color="secondary.main">
-                            video!
-                        </Typography> 
-
-                    </Box>
-                    </Grid>
-                    <Grid item width="1000%">
-                        <Box sx={{backgroundColor: "text.primary", height: 900, borderRadius: '30px'}} px={50}>
-
-                        </Box>
-                    </Grid>
+                    
+                    
                 </Grid>
-                
-
-            </Box>
         </Box>
     )
 }
