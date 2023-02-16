61bca56cd3d0ddc3df5ffad45a96ed6610747d81
thejulia1 <90807530+thejulia1@users.noreply.github.com>
2023-02-16 01:18:19 -0500

    Middle section of mission page

--- src/components/missionPage/SecondSection.tsx
+++ src/components/missionPage/SecondSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Paper, useMediaQuery, useTheme, Grid } from "@mui/material";
+import { Box, Typography, Paper, useMediaQuery, useTheme, Grid , Divider } from "@mui/material";
 import { useState } from "react";
 import { theme } from "../../styles/theme";
 
@@ -6,31 +6,76 @@ export default function SecondSection() {
 
     const theme = useTheme()
     const smallScreen = useMediaQuery(theme.breakpoints.down('md'))
+    const medScreen = useMediaQuery(theme.breakpoints.down('lg'))
+    const xsmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
+    const xlScreen =  useMediaQuery(theme.breakpoints.down('xl'))
 
     return (
-        <Box  sx={{ background:`linear-gradient(to bottom,#F2F5FA,#9e9e9e)`, height: 2200,backgroundPosition: 'center', backgroundRepeat: 'no-repeat',backgroundSize: 'cover',position: 'relative'}}>
+        <Box  sx={{ background:`linear-gradient(to bottom,#F2F5FA,#9e9e9e)`, height: 2300,backgroundPosition: 'center', backgroundRepeat: 'no-repeat',backgroundSize: 'cover',position: 'relative'}}>
             <Grid container spacing={0} alignItems="center" direction="row">
                     <Grid item >
                         <Box sx={{height: 500}} textAlign="left" >
-                            <Typography width="50%" pl={40}  pt={15}   sx={{ letterSpacing: 1}} variant={smallScreen ? 'h5' : 'h4'} color="secondary.main">
+                            <Typography width={smallScreen? "100%": "50%"} pl={smallScreen? 10: medScreen? 20: 30}  pt={15}   sx={{ letterSpacing: 1}} variant={smallScreen ? 'h5' : 'h4'} color="secondary.main">
                             Learning From the Best
                             </Typography> 
-                            <Typography  width="50%"  pl={40} pt={5}  sx={{  }} variant={smallScreen ?  'body2' : 'body1'} color="secondary.main">
+                            <Typography  width={smallScreen? "100%": "50%"}  pl={smallScreen? 10: medScreen? 20: 30}  pt={5}  sx={{  }} variant={smallScreen ?  'body2' : 'body1'} color="secondary.main">
                             For centuries, farmers have used their experience to learn and adapt to the needs of their animals. 
                             Their dedication to caring for animals helps to feed the world.
                             </Typography> 
-                            <Typography width="50%" pl={40} pt={5}  sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
+                            <Typography width={smallScreen? "100%": "50%"} pl={smallScreen? 10: medScreen? 20: 30}  pt={5}  sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
                             The amount of specialized knowledge needed to be a poultry farmer is surprisingly great. Just by 
                             hearing one of their chickens, a poultry farmer can tell if they’re healthy, happy, hungry, or sick. 
                             It may seem instinctual to some, but that level of adaptation is made possible by the supercomputer
                             between our ears -- our brains!
                             </Typography> 
-                            <Typography width="50%" pl={40} pt={5}  pb={15} sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
+                            <Typography width={smallScreen? "100%": "50%"} pl={smallScreen? 10: medScreen? 20: 30}  pt={5}  pb={15} sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="secondary.main">
                             We can’t replicate what farmers do, but we can strive toke ma their work easier in this rapidly changing world.
                             </Typography> 
                             
                             <Grid item width="100%" px={10}>
-                                <Box sx={{backgroundColor: "text.primary", height: 750, borderRadius: '30px'}} px={50}>
+                                <Box  py={smallScreen? 3 :xsmallScreen? 5: medScreen? 10: 12} sx={{backgroundColor: "text.primary", borderRadius: '30px'}} px={smallScreen? 1: medScreen? 5: 10} height={smallScreen? 1000: medScreen? 900: 750}>
+                                    <Divider  sx={{backgroundColor: 'white'}}>
+                                      
+                                    </Divider>
+
+                                    <Typography width="100%" textAlign="left" pt={2} sx={{  }} variant={smallScreen ? 'h5' : 'h4'} color="primary.main">
+                                    Poultry Farming in a Modern Age
+                                    </Typography> 
+                                    <Typography width="100%" textAlign="left" pt={5} sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="white">
+                                    Over the past 50 years, the average size of broiler houses has doubled to compensate for increased demand from retailers
+                                     in the poultry industry. Year by year, commercial poultry farmers are expected to produce more meat to fulfill that demand,
+                                      which necessitates the ability to care for more chickens.
+                                    </Typography> 
+                                    <Grid container spacing={1} direction={smallScreen? "column": "row"}>
+                                        <Grid item md={6} xs={10}>
+                                            <Typography width={smallScreen? "100%":"100%"} textAlign="left" pt={5} sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="white">
+                                            This dramatic increase in production has made it more difficult for poultry farmers to actively monitor the welfare of 
+                                            their chickens. Modern poultry farms can house birds numbering in the tens of thousands -- in a setting like this, it’s 
+                                            hard, even for skilled farmers, to pay attention to the needs of any particular chicken. 
+                                            </Typography> 
+                                            <Typography width={smallScreen? "100%":"100%"} textAlign="left" pt={5} pb={5} sx={{  }} variant={smallScreen ? 'body2' : 'body1'} color="white">
+                                            Additionally, contract growers are often obligated to submit a detailed report on their welfare practices and outcomes 
+                                            to their integrators in order to keep their jobs. There’s a lot of pressure on farmers to do their absolute best to satisfy 
+                                            the industry and to take good care of their animals at the same time, which is a difficult balancing act to achieve.
+                                        </Typography>
+
+                                        </Grid>
+                                        <Grid item md={6} xs={10}>
+                                            <Box py={smallScreen? 1: 1} px={xsmallScreen? 10:smallScreen? 20: medScreen? 0: 10}>
+                                                <img 
+                                                style={{aspectRatio: '11/9', width:'90%', height: 'auto'}}
+                                                src="/audiotstat1.png" />
+                                            </Box>  
+
+                                        </Grid>
+                                       
+                                       
+                                    </Grid>
+                                    <Divider  sx={{backgroundColor: 'white'}}>
+                                      
+                                      </Divider>
+                                    
+                                    
 
                                 </Box>
                             </Grid>
