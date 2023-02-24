cc033899bf4eb62df4a6ad8f6ff6b6ef2a738ba0
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2023-02-24 11:19:51 -0500

    Added component for contact page content

---
+++ src/components/contact/ContactBox.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import TextField from "../formik/TextField";
+
+export default function ContactBox() {
+
+    return (
+        <Box>       
+
+        </Box>
+    )
+    
+}
