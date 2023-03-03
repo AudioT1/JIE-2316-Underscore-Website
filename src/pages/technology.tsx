e65a42fa3e4d4e74361f935a935aec854a676440
thejulia1 <90807530+thejulia1@users.noreply.github.com>
2023-03-02 20:14:08 -0500

    debug

--- src/pages/technology.tsx
+++ src/pages/technology.tsx
@@ -2,7 +2,7 @@
 import Head from "next/head";
 import { useAnalytics } from "../utils/analytics";
 import Header from "../components/nav/Header";
-import Composite from "../components/TechPage/Composite"
+import Composite from "../components/techPage/Composite";
 
 export default function Technology() {
 
