9f6d7b1e8bfce5ddd20e7225f43d233684039764
thejulia1 <90807530+thejulia1@users.noreply.github.com>
2023-03-09 22:01:07 -0500

    tech page design edits

--- src/components/techPage/ComponentsModels.tsx
+++ src/components/techPage/ComponentsModels.tsx
@@ -22,5 +22,101 @@ export default function ComponentsModels() {
         }
     }
 
-    return ()
+    return (
+        <Box >
+            <Typography  py={0} sx={{ fontWeight: 'bold' }} my={15} variant={medScreen ? 'h3' : 'h2'} color="secondary.main" textAlign="center">
+                Components And Models
+            </Typography> 
+            <Grid container spacing={12} direction="row" >
+                <Grid item md={4} xs={12}>
+                    <Box textAlign="center">
+                    
+
+                    <img 
+                        style={{aspectRatio: '11/9', width:'40%', height: 'auto'}}
+                        src="/babyChick.png" />
+                        <Typography pb={5}  sx={{ fontWeight: 'bold' }} variant={medScreen ? 'h4' : 'h3'} color="black" >
+                                YOUNG
+                        </Typography> 
+                        <Typography   py={2} pl={medScreen? 0:5}  textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black" >
+                            Distess
+                        </Typography> 
+                        <Typography  py={2} pl={medScreen? 0:5} textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Day
+                        </Typography> 
+                        <Typography   py={2} pl={medScreen? 0:5} textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Night
+                        </Typography> 
+                        <Typography  py={2} pl={medScreen? 0:5}  textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Water
+                        </Typography> 
+                        <Typography   py={2}pl={medScreen? 0:5} textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black" >
+                            Anomaly
+                        </Typography> 
+
+                </Box>
+            </Grid>
+            <Grid item md={4} xs={12}>
+                <Box mt={smallScreen? 15:0} textAlign="center" >
+                    <img 
+                        style={{aspectRatio: '11/9', width:'40%', height: 'auto'}}
+                        src="/midChick.png" />
+                        <Typography pb={5}  sx={{ fontWeight: 'bold' }} variant={medScreen ? 'h4' : 'h3'} color="black"  >
+                                MID
+                        </Typography> 
+                        <Typography   py={2} pl={medScreen? 0:6}  textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black" >
+                            Distess
+                        </Typography> 
+                        <Typography  py={2} pl={medScreen? 0:6}    textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Day
+                        </Typography> 
+                        <Typography   py={2} pl={medScreen? 0:6}    textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Night
+                        </Typography> 
+                        <Typography  py={2}   pl={medScreen? 0:6}    textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Water
+                        </Typography> 
+                        <Typography   py={2}  pl={medScreen? 0:6}   textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black" >
+                            Anomaly
+                        </Typography> 
+
+                    </Box>
+
+            </Grid>
+            <Grid item md={4} xs={12}>
+            <Box mt={smallScreen? 15:0} textAlign="center" pb={15}>
+                    <img 
+                        style={{aspectRatio: '11/9', width:'40%', height: 'auto'}}
+                        src="/bigChick.png" />
+                        <Typography  pb={5} sx={{ fontWeight: 'bold' }} variant={medScreen ? 'h4' : 'h3'} color="black">
+                                LATE
+                        </Typography> 
+                        <Typography   py={2} pl={medScreen? 0:5} textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black" >
+                            Distess
+                        </Typography> 
+                        <Typography  py={2} pl={medScreen? 0:5}  textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Day
+                        </Typography> 
+                        <Typography   py={2} pl={medScreen? 0:5}  textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Night
+                        </Typography> 
+                        <Typography  py={2}  pl={medScreen? 0:5}  textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black">
+                            Water
+                        </Typography> 
+                        <Typography   py={2} pl={medScreen? 0:5} textAlign="left" mx={smallScreen? 15:10}   variant={medScreen ? 'h4' : 'h3'} color="black" >
+                            Anomaly
+                        </Typography> 
+
+                    </Box>
+
+            </Grid>
+
+
+            
+         </Grid>
+        
+
+          
+    </Box>
+    )
 }
