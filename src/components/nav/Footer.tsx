8183dd1738c1fb95ec948032454170897aa91e09
Manuel Roglan <manueljoseph113@gmail.com>
2023-04-20 21:40:40 -0400

    added footer to pages

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -1,16 +1,26 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import Link from "next/link";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Footer() {
 
     return (
         <Box bgcolor="background.paper">
             <Box my={1} textAlign="center">
-                <Typography variant="body1">
-                    <Link href="/news">
-                        Find us on LinkedIn!
-                    </Link>
-                </Typography>
+                <Grid container spacing={1} justifyContent="center" alignContent="center">
+                    <Grid item>
+                        <Link href="/news">
+                            <LinkedInIcon />
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="body1">
+                            <Link href="/news">
+                                Find us on LinkedIn!
+                            </Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Box>
         </Box>
     )
